fb656ad504e5c8c38b9dd4282883d7eb4ab75458
gavc <3795572+gavc@users.noreply.github.com>
2025-04-18 11:52:01 +0000

    Updates

---
+++ libs/holidays.js
@@ -0,0 +1,52 @@
+// libs/holidays.js
+// Queensland Public Holidays 2025-2027
+window.PUBLIC_HOLIDAYS = [
+    // 2025
+    { date: "2025-01-01", name: "New Year's Day" },
+    { date: "2025-01-27", name: "Australia Day (observed)" },
+    { date: "2025-04-18", name: "Good Friday" },
+    { date: "2025-04-19", name: "The day after Good Friday" },
+    { date: "2025-04-20", name: "Easter Sunday" },
+    { date: "2025-04-21", name: "Easter Monday" },
+    { date: "2025-04-25", name: "Anzac Day" },
+    { date: "2025-05-05", name: "Labour Day" },
+    { date: "2025-08-13", name: "Royal Queensland Show (Brisbane area only)" },
+    { date: "2025-10-06", name: "King’s Birthday" },
+    { date: "2025-12-24", name: "Christmas Eve (6pm–midnight)" },
+    { date: "2025-12-25", name: "Christmas Day" },
+    { date: "2025-12-26", name: "Boxing Day" },
+  
+    // 2026
+    { date: "2026-01-01", name: "New Year's Day" },
+    { date: "2026-01-26", name: "Australia Day" },
+    { date: "2026-04-03", name: "Good Friday" },
+    { date: "2026-04-04", name: "The day after Good Friday" },
+    { date: "2026-04-05", name: "Easter Sunday" },
+    { date: "2026-04-06", name: "Easter Monday" },
+    { date: "2026-04-25", name: "Anzac Day" },
+    { date: "2026-05-04", name: "Labour Day" },
+    { date: "2026-08-12", name: "Royal Queensland Show (Brisbane area only)" },
+    { date: "2026-10-05", name: "King’s Birthday" },
+    { date: "2026-12-24", name: "Christmas Eve (6pm–midnight)" },
+    { date: "2026-12-25", name: "Christmas Day" },
+    { date: "2026-12-26", name: "Boxing Day" },
+    { date: "2026-12-28", name: "Boxing Day (observed)" },
+  
+    // 2027
+    { date: "2027-01-01", name: "New Year's Day" },
+    { date: "2027-01-26", name: "Australia Day" },
+    { date: "2027-03-26", name: "Good Friday" },
+    { date: "2027-03-27", name: "The day after Good Friday" },
+    { date: "2027-03-28", name: "Easter Sunday" },
+    { date: "2027-03-29", name: "Easter Monday" },
+    { date: "2027-04-26", name: "Anzac Day (observed)" },
+    { date: "2027-05-03", name: "Labour Day" },
+    { date: "2027-08-11", name: "Royal Queensland Show (Brisbane area only)" },
+    { date: "2027-10-04", name: "King’s Birthday" },
+    { date: "2027-12-24", name: "Christmas Eve (6pm–midnight)" },
+    { date: "2027-12-25", name: "Christmas Day" },
+    { date: "2027-12-26", name: "Boxing Day" },
+    { date: "2027-12-27", name: "Christmas Day (observed)" },
+    { date: "2027-12-28", name: "Boxing Day (observed)" }
+  ];
+  
